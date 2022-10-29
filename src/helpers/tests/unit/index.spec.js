fa9cbb73f360b78417a3665a5af49a6c53debaac
officialp123 <96476199+officialp123@users.noreply.github.com>
2022-10-29 19:24:27 +0100

    Add files via upload

---
+++ src/helpers/tests/unit/index.spec.js
@@ -0,0 +1,17 @@
+import { convertToArray, getParagraphs } from "../../index";
+
+import jsonData from "../../../data/english.json";
+
+import { expected1, expected2 } from "./data";
+
+describe("JsonFormatter Util", () => {
+  test("TESTING IF WE RECIEVED EXPECTED DATA", () => {
+     expect(convertToArray(jsonData)).toEqual(expected1);
+  });
+});
+
+describe("TESTING SECOND ALGORITHM", () => {
+  test("TESTING IF WE RECIEVED EXPECTED DATA", () => {
+    expect(getParagraphs(jsonData)).toMatchObject(expected2);
+  });
+});
